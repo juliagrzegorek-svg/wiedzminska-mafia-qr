20b7aada52e497334e9e09053f442cd3b15df113
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-17 23:55:45 +0200

    Create monsters.js

---
+++ src/data/monsters.js
@@ -0,0 +1,7 @@
+export const MONSTERS = [
+  { id:'bruxa', name:'Bruxa', what:'Wampirzyca', img:'/monsters/bruxa.jpg', abilityKey:'bite' },
+  { id:'vampire', name:'Wampir', what:'Krwiopijca', img:'/monsters/vampire.jpg', abilityKey:'bite' },
+  { id:'werewolf', name:'Wilkołak', what:'Watażka Nocy', img:'/monsters/werewolf.jpg', abilityKey:'scratch' },
+  { id:'striga', name:'Strzyga', what:'Przeklęta', img:'/monsters/striga.jpg', abilityKey:'maul' },
+  { id:'wraith', name:'Upiór', what:'Zjawa', img:'/monsters/wraith.jpg', abilityKey:'haunt' },
+]
