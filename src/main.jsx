f7411ef8f5679f795a539a117090cd65cac836ec
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-17 03:27:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import WitcherDeck from "./components/WitcherDeck.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createRoot(document.getElementById("root")).render(<WitcherDeck />);
