bfd3972ab8fb9af49ef5cbcd636cdd0180603ea6
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-17 18:13:27 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-// src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import WitcherDeck from "./components/WitcherDeck.jsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(<WitcherDeck />);
