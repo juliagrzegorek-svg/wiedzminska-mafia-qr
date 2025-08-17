1fe9d64b413fd1683502eaeb98934c4ecba0a957
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-17 19:40:47 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,5 @@
-/**** Tailwind config ****/
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
   plugins: [],
 };
