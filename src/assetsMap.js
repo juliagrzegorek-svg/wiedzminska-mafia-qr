c938265f9af61fa2f7c200fb45b3b3657e6cb570
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-19 13:58:11 +0200

    Create assetsMap.js

---
+++ src/assetsMap.js
@@ -0,0 +1,30 @@
+// ===== Bohaterowie =====
+import yennefer  from './assets/characters/yennefer.png';
+import ciri      from './assets/characters/ciri.png';
+import keira     from './assets/characters/keira.png';
+import filippa   from './assets/characters/filippa.png';
+import margarita from './assets/characters/margarita.png';
+import nenneke   from './assets/characters/nenneke.png';
+import triss     from './assets/characters/triss.png';
+import shani     from './assets/characters/shani.png';
+import fringilla from './assets/characters/fringilla.png';
+import geralt    from './assets/characters/geralt.png';
+import jaskier   from './assets/characters/jaskier.png';
+import zoltan    from './assets/characters/zoltan.png';
+import emhyr     from './assets/characters/emhyr.png';
+import vernon    from './assets/characters/vernon.png';
+import avallach  from './assets/characters/avallach.png';
+
+// ===== Potwory =====
+import upior     from './assets/monsters/upior.png';
+import strzyga   from './assets/monsters/strzyga.png';
+import wilkolak  from './assets/monsters/wilkolak.png';
+import wampir    from './assets/monsters/wampir.png';
+import bruxa     from './assets/monsters/bruxa.png';
+
+export const CHAR_IMG = {
+  yennefer, ciri, keira, filippa, margarita, nenneke, triss, shani, fringilla,
+  geralt, jaskier, zoltan, emhyr, vernon, avallach
+};
+
+export const MONSTER_IMG = { upior, strzyga, wilkolak, wampir, bruxa };
