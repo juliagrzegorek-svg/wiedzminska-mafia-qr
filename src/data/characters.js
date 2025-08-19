e3b24f6d8f937573020dba55d79b6896eca0dc73
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-19 21:32:56 +0200

    Update characters.js

--- src/data/characters.js
+++ src/data/characters.js
@@ -1,20 +1,18 @@
-import { CHAR_IMG } from '../assetsMap.js';
-
+// Prosty katalog bohaterów z JEDNOZNACZNYM adresem obrazka
 export const CHARACTERS = [
-  { id:'yennefer',  name:'Yennefer',        sex:'K', role:'Bohaterka', what:'Uzdrowicielka',  img:CHAR_IMG.yennefer,  abilityKey:'yen-healer' },
-  { id:'ciri',      name:'Ciri',            sex:'K', role:'Bohaterka', what:'Jasnowidzka',    img:CHAR_IMG.ciri,      abilityKey:'ciri-seer' },
-  { id:'keira',     name:'Keira Metz',      sex:'K', role:'Bohaterka', what:'Czarodziejka',   img:CHAR_IMG.keira,     abilityKey:'citizen' },
-  { id:'filippa',   name:'Filippa Eilhart', sex:'K', role:'Bohaterka', what:'Magini',         img:CHAR_IMG.filippa,   abilityKey:'filippa-revive' },
-  { id:'margarita', name:'Margarita Laux-Antille', sex:'K', role:'Bohaterka', what:'Czarodziejka', img:CHAR_IMG.margarita, abilityKey:'citizen' },
-  { id:'nenneke',   name:'Nenneke',         sex:'K', role:'Bohaterka', what:'Kapłanka',       img:CHAR_IMG.nenneke,   abilityKey:'nenneke-antivamp' },
-  { id:'triss',     name:'Triss Merigold',  sex:'K', role:'Bohaterka', what:'Czarodziejka',   img:CHAR_IMG.triss,     abilityKey:'citizen' },
-  { id:'shani',     name:'Shani',           sex:'K', role:'Bohaterka', what:'Lekarka',        img:CHAR_IMG.shani,     abilityKey:'citizen' },
-  { id:'fringilla', name:'Fringilla Vigo',  sex:'K', role:'Bohaterka', what:'Czarodziejka',   img:CHAR_IMG.fringilla, abilityKey:'citizen' },
-
-  { id:'geralt',    name:'Geralt',          sex:'M', role:'Bohater',   what:'Jasnowidz',      img:CHAR_IMG.geralt,    abilityKey:'geralt-seer' },
-  { id:'jaskier',   name:'Jaskier',         sex:'M', role:'Bohater',   what:'Bard',           img:CHAR_IMG.jaskier,   abilityKey:'bard-cancel-vote' },
-  { id:'zoltan',    name:'Zoltan',          sex:'M', role:'Bohater',   what:'Wojownik',       img:CHAR_IMG.zoltan,    abilityKey:'zoltan-shield-village' },
-  { id:'emhyr',     name:'Emhyr',           sex:'M', role:'Bohater',   what:'Cesarz',         img:CHAR_IMG.emhyr,     abilityKey:'emhyr-pardon' },
-  { id:'vernon',    name:'Vernon Roche',    sex:'M', role:'Bohater',   what:'Żołnierz',       img:CHAR_IMG.vernon,    abilityKey:'citizen' },
-  { id:'avallach',  name:"Avallac’h",       sex:'M', role:'Bohater',   what:'Mędrzec',        img:CHAR_IMG.avallach,  abilityKey:'citizen' },
+  { id: 'yennefer',  name: 'Yennefer',                sex: 'K', role: 'Bohaterka', what: 'Czarodziejka', img: '/assets/yennefer.png' },
+  { id: 'ciri',      name: 'Ciri',                     sex: 'K', role: 'Bohaterka', what: 'Jasnowidzka',  img: '/assets/ciri.png' },
+  { id: 'keira',     name: 'Keira Metz',               sex: 'K', role: 'Bohaterka', what: 'Czarodziejka', img: '/assets/keira.png' },
+  { id: 'filippa',   name: 'Filippa Eilhart',          sex: 'K', role: 'Bohaterka', what: 'Czarodziejka', img: '/assets/filippa.png' },
+  { id: 'margarita', name: 'Margarita Laux-Antille',   sex: 'K', role: 'Bohaterka', what: 'Rektorka',     img: '/assets/margarita.png' },
+  { id: 'nenneke',   name: 'Nenneke',                  sex: 'K', role: 'Bohaterka', what: 'Kapłanka',     img: '/assets/nenneke.png' },
+  { id: 'triss',     name: 'Triss Merigold',           sex: 'K', role: 'Bohaterka', what: 'Czarodziejka', img: '/assets/triss.png' },
+  { id: 'shani',     name: 'Shani',                    sex: 'K', role: 'Bohaterka', what: 'Lekarka',      img: '/assets/shani.png' },
+  { id: 'fringilla', name: 'Fringilla Vigo',           sex: 'K', role: 'Bohaterka', what: 'Czarodziejka', img: '/assets/fringilla.png' },
+  { id: 'geralt',    name: 'Geralt',                   sex: 'M', role: 'Bohater',   what: 'Wiedźmin',     img: '/assets/geralt.png' },
+  { id: 'jaskier',   name: 'Jaskier',                  sex: 'M', role: 'Bohater',   what: 'Bard',         img: '/assets/jaskier.png' },
+  { id: 'zoltan',    name: 'Zoltan',                   sex: 'M', role: 'Bohater',   what: 'Wojownik',     img: '/assets/zoltan.png' },
+  { id: 'emhyr',     name: 'Emhyr',                    sex: 'M', role: 'Bohater',   what: 'Cesarz',       img: '/assets/emhyr.png' },
+  { id: 'vernon',    name: 'Vernon Roche',             sex: 'M', role: 'Bohater',   what: 'Żołnierz',     img: '/assets/vernon.png' },
+  { id: 'avallach',  name: "Avallac'h",                sex: 'M', role: 'Bohater',   what: 'Mędrzec',      img: '/assets/avallach.png' },
 ];
