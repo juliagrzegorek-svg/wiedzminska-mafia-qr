014dac54f045678b48fc7df4f4c0f1112294d555
juliagrzegorek-svg <julia.grzegorek@gmail.com>
2025-08-19 13:58:43 +0200

    Update monsters.js

--- src/data/monsters.js
+++ src/data/monsters.js
@@ -1,7 +1,9 @@
+import { MONSTER_IMG } from '../assetsMap.js';
+
 export const MONSTERS = [
-  { id:'upior',    name:'Upiór',    what:'Przerażająca zjawa',   img:'/assets/monsters/upior.png' },
-  { id:'strzyga',  name:'Strzyga',  what:'Przeklęta bestia',     img:'/assets/monsters/strzyga.png' },
-  { id:'wilkolak', name:'Wilkołak', what:'Bestia księżyca',      img:'/assets/monsters/wilkolak.png' },
-  { id:'wampir',   name:'Wampir',   what:'Krwiopijca nocy',      img:'/assets/monsters/wampir.png' },
-  { id:'bruxa',    name:'Bruxa',    what:'Wampirzyca-zabójczyni',img:'/assets/monsters/bruxa.png' },
+  { id:'upior',    name:'Upiór',    what:'Przerażająca zjawa',   img:MONSTER_IMG.upior },
+  { id:'strzyga',  name:'Strzyga',  what:'Przeklęta bestia',     img:MONSTER_IMG.strzyga },
+  { id:'wilkolak', name:'Wilkołak', what:'Bestia księżyca',      img:MONSTER_IMG.wilkolak },
+  { id:'wampir',   name:'Wampir',   what:'Krwiopijca nocy',      img:MONSTER_IMG.wampir },
+  { id:'bruxa',    name:'Bruxa',    what:'Wampirzyca-zabójczyni',img:MONSTER_IMG.bruxa },
 ];
